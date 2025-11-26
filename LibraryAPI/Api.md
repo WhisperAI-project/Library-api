761896187be11eef3251008fd6dcb30470e6299c
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-11-26 15:58:11 +0100

    added db contect and entityframwork nuget packages

---
+++ LibraryAPI/Api.md
@@ -0,0 +1,53 @@
+# API-mall
+
+Endpoint: Få en lista över alla böcker
+
+Metod: [HTTP-Get]
+
+Sökväg: [/Api/Books]
+
+Request Body:
+
+Inget innehåll krävs för denna förfrågan.
+```
+
+Svar (Framgång):
+
+```json
+{
+  Books: [
+	{
+	  Id: 1,
+	  Title: "Exempelbok 1",
+	  IsAvailable: true
+
+}
+```
+
+Statuskoder:
+
+- 200: Framgång
+- 400: Felaktig förfrågan
+- etc.
+
+
+Endpoint: 
+
+Metod: [HTTP]
+
+Sökväg: []
+
+Request Body:
+
+
+```
+
+Svar (Framgång):
+
+```
+
+Statuskoder:
+
+- 200: Framgång
+- 400: Felaktig förfrågan
+- etc.
---
+++ LibraryAPI/LibraryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryAPI
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Entities.Book> Books { get; set; }
+        public DbSet<Entities.User> Users { get; set; }
+        public DbSet<Entities.Loan> Loans { get; set; }
+        public DbSet<Entities.Library> Libraries { get; set; }   
+    }
+}
